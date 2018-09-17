e5889626587a9f969adb5ac74da5115e653c89dd
Mikhail Noskov <nm@perm-tools.ru>
2018-09-17 06:21:35 +0000

    new autoloader

--- assets/plugins/commerce/autoload.php
+++ assets/plugins/commerce/autoload.php
@@ -5,24 +5,6 @@ spl_autoload_register(function ($class) {
 
     if ($classes === null) {
         $classes = [
-            'Commerce\\Commerce'                    => '/src/Commerce.php',
-            'Commerce\\SettingsTrait'               => '/src/SettingsTrait.php',
-            'Commerce\\CartsManager'                => '/src/CartsManager.php',
-            'Commerce\\Interfaces\\Cart'            => '/src/Interfaces/Cart.php',
-            'Commerce\\Interfaces\\Payment'         => '/src/Interfaces/Payment.php',
-            'Commerce\\Interfaces\\Processor'       => '/src/Interfaces/Processor.php',
-            'Commerce\\Carts\\DocListerTrait'       => '/src/Carts/DocListerTrait.php',
-            'Commerce\\Carts\\DocListerCart'        => '/src/Carts/DocListerCart.php',
-            'Commerce\\Carts\\DocListerOrderCart'   => '/src/Carts/DocListerOrderCart.php',
-            'Commerce\\Carts\\DocListerItemsList'   => '/src/Carts/DocListerItemsList.php',
-            'Commerce\\Carts\\SessionCart'          => '/src/Carts/SessionCart.php',
-            'Commerce\\Carts\\SimpleCart'           => '/src/Carts/SimpleCart.php',
-            'Commerce\\Payments\\Payment'           => '/src/Payments/Payment.php',
-            'Commerce\\Processors\\SimpleProcessor' => '/src/Processors/SimpleProcessor.php',
-
-            'Commerce\\Payments\\SberbankPayment'    => '/src/Payments/SberbankPayment.php',
-            'Commerce\\Payments\\YandexkassaPayment' => '/src/Payments/YandexkassaPayment.php',
-
             'FormLister\\Order'     => '/src/Controllers/Order.php',
             'FormLister\\Form'      => '/../../snippets/FormLister/core/controller/Form.php',
             'FormLister\\Core'      => '/../../snippets/FormLister/core/FormLister.abstract.php',
@@ -32,11 +14,24 @@ spl_autoload_register(function ($class) {
             'FormLister\\Validator' => '/../../snippets/FormLister/lib/Validator.php',
             'Helpers\\Mailer'       => '/../../lib/Helpers/Mailer.php',
             'Helpers\\Config'       => '/../../lib/Helpers/Config.php',
+            'Helpers\\Debug'        => '/../../snippets/FormLister/lib/Debug.php',
             'APIhelpers'            => '/../../lib/APIHelpers.class.php',
         ];
     }
 
     if (isset($classes[$class])) {
         require __DIR__ . $classes[$class];
+        return;
+    }
+
+    if (strpos($class, 'Commerce\\') === 0) {
+        $parts = explode('\\', $class);
+        array_shift($parts);
+        
+        $filename = __DIR__ . '/src/' . implode('/', $parts) . '.php';
+
+        if (is_readable($filename)) {
+            require $filename;
+        }
     }
 }, true);
