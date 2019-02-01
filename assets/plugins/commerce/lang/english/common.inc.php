968b49fc23bc7110a741ba5b4b2469cff1c38940
Marc <esens-2.5@ESENSs-iMac.fritz.box>
2019-02-01 15:35:48 +0100

    Add english translation

---
+++ assets/plugins/commerce/lang/english/common.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+setlocale(LC_ALL, 'en_US.UTF-8');
+
+return [
+     'measures.units' => 'items',
+    
+     'currency.title' => 'Dollar',
+     'currency.code' => 'USD',
+     'currency.right_symbol' => '$',
+     'currency.decimals' => '2',
+     'currency.decimals_separator' => '.',
+     'currency.thousands_separator' => ',',
+];
---
+++ assets/plugins/commerce/lang/english/menu.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+setlocale(LC_ALL, 'en_US.UTF-8');
+
+return [
+    'menu.commerce' => 'Shop',
+    'menu.orders' => 'Orders',
+    'menu.statuses' => 'Order status',
+    'menu.currency' => 'Currency',
+];
