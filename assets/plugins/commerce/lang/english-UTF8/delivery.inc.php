1f21aa3f4e2e48cee666f9ee2329db3cf7db98cc
Marc <esens-2.5@ESENSs-iMac.fritz.box>
2019-02-01 15:35:15 +0100

    Revert "Add NL + EN language"
    This reverts commit bc9cb80c26e0fa7580fb1e9240becc4e50629e44.

--- assets/plugins/commerce/lang/english-UTF8/delivery.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-setlocale(LC_ALL, 'en_US.UTF-8');
-
-return [
-    'delivery.caption' => 'Delivery method',
-    'delivery.fixed_title' => 'Delivery around the city',
-    'delivery.pickup_title' => 'Pickup',
-];
--- assets/plugins/commerce/lang/nederlands-UTF8/delivery.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-setlocale(LC_ALL, 'nl_NL.UTF-8');
-
-return [
-    'delivery.caption' => 'Bezorgmethode',
-    'delivery.fixed_title' => 'Bezorging in de buurt',
-    'delivery.pickup_title' => 'Ophalen',
-];
