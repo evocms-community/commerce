c35a261967975829ab9342e75b91ea13e4b518da
Pathologic <m@xim.name>
2022-05-13 15:37:54 +0300

    polish language

---
+++ assets/plugins/commerce/lang/polish-utf8/datepicker.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'dp.apply_label'        => 'Wybrać',
+    'dp.cancel_label'       => 'Wykasować',
+    'dp.custom_range_label' => 'Własny odstęp',
+    'dp.today'              => 'Dziesiaj',
+    'dp.yesterday'          => 'Wczoraj',
+    'dp.last_week'          => 'Ostatni 7 dni',
+    'dp.last_month'         => 'Ostatni 30 dni',
+    'dp.this_month'         => 'Ten miesiąc',
+    'dp.prev_month'         => 'Poprzedni miesiąc',
+    'dp.days_of_week'       => ['Niedz', 'Pon', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob'],
+    'dp.month_names'        => ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+];
---
+++ assets/plugins/commerce/lang/polish-utf8/menu.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+
+return [
+    'menu.commerce' => 'Sklep',
+    'menu.orders' => 'Zamówienia',
+    'menu.statuses' => 'Statusy zamówień',
+    'menu.currency' => 'Waluty',
+];
